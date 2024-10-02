b6e8c47fb4b2b4f0198ab1a81706608dd185b3ef
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-10-02 18:46:44 +0100

    Add charts

--- src/components/uniswapV2Pool/PoolChartsContainer.tsx
+++ src/components/uniswapV2Pool/PoolChartsContainer.tsx
@@ -1,9 +1,20 @@
-import { HStack, Text } from "@chakra-ui/react"
+import { HStack, Text, Box } from "@chakra-ui/react"
+import { BarChart, Bar, XAxis, YAxis, Cell, ResponsiveContainer } from "recharts"
 
 export default function PoolChartsContainer() {
+    const data = [
+        { name: "token0", tokenValue: 1000 },
+        { name: "token1", tokenValue: 2000 },
+    ]
+
+    const colors = {
+        token0: "#0fc546",
+        token1: "#c16c26",
+    }
+
     return (
-        <HStack w={"100%"} justifyContent={"start"} alignItems={"start"} gap={10} py={3} px={3}>
-            <Text
+        <HStack w={"100%"} minH={"200px"} justifyContent={"start"} alignItems={"start"} gap={10} py={3} px={3}>
+            {/* <Text
                 fontSize={"lg"}
                 fontWeight={"semibold"}
                 className={"bgPage"}
@@ -14,7 +25,21 @@ export default function PoolChartsContainer() {
                 w={"fit-content"}
             >
                 Pool Charts
-            </Text>
+            </Text> */}
+
+            <Box w="200px" h="200px">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart data={data}>
+                        <XAxis dataKey="name" />
+                        <YAxis tickFormatter={(value) => `$${value}`} />
+                        <Bar dataKey="tokenValue" radius={[10, 10, 0, 0]}>
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[entry.name]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </ResponsiveContainer>
+            </Box>
         </HStack>
     )
 }
