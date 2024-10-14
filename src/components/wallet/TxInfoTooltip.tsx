1b3490a0b9a6e16f26f5414d430c5becf2808c22
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-10-14 16:18:52 +0100

    Update tooltip styles

--- src/components/wallet/TxInfoTooltip.tsx
+++ src/components/wallet/TxInfoTooltip.tsx
@@ -43,7 +43,7 @@ export default function TxInfoTooltip() {
                 gutter={12}
                 label={
                     <VStack className="tooltipLabel" alignItems={"center"} fontWeight={"semibold"}>
-                        <VStack className="bgPage" py={1} px={6} borderRadius={"full"} gap={1} fontWeight={"bold"}>
+                        <VStack className="bgPage" py={1} px={5} borderRadius={"full"} gap={1} fontWeight={"bold"} textAlign={"center"}>
                             <HStack>
                                 <FontAwesomeIcon icon={faInfoCircle} size={"lg"} />
                                 <Text>TESTNET ONLY</Text>
@@ -51,16 +51,16 @@ export default function TxInfoTooltip() {
                             </HStack>
                             <Text>No real money is ever used in this playground</Text>
                         </VStack>
-                        <Text textAlign={"center"} fontSize={"md"} fontWeight={"bold"} w={"100%"}>
-                            This single transaction performs all the following actions
+                        <Text fontSize={"15px"} fontWeight={"bold"} w={"100%"}>
+                            This single transaction performs ALL the following actions
                         </Text>
                         <VStack alignItems={"start"} w={"100%"}>
-                            <Text>1. Creates a new ERC20 Token called Diamond 💎</Text>
-                            <Text>2. Creates a new ERC20 Token called Wood 🪵</Text>
-                            <Text>3. Creates a new ERC20 Token called Stone 🪨</Text>
-                            <Text>4. Creates a new UniswapV2 Pool Diamond/Wood + adds liquidity</Text>
-                            <Text>5. Creates a new UniswapV2 Pool Diamond/Stone + adds liquidity</Text>
-                            <Text>6. Creates a new UniswapV2 Pool Wood/Stone + adds liquidity</Text>
+                            <Text>1. Creates a new ERC20 token called Diamond 💎</Text>
+                            <Text>2. Creates a new ERC20 token called Wood 🪵</Text>
+                            <Text>3. Creates a new ERC20 token called Stone 🪨</Text>
+                            <Text>4. Creates a new UniswapV2 pool Diamond/Wood + adds liquidity</Text>
+                            <Text>5. Creates a new UniswapV2 pool Diamond/Stone + adds liquidity</Text>
+                            <Text>6. Creates a new UniswapV2 pool Wood/Stone + adds liquidity</Text>
                             <Text>7. Sends the initial balance of Diamond, Wood and Stone to the user </Text>
                         </VStack>
                     </VStack>
